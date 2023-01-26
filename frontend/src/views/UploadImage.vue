55694a1e1ae09ebcd6706385dacd27bbe364d207
Adeola Ephraim <62262920+codeDeeAi@users.noreply.github.com>
2023-01-26 11:11:58 +0100

    P2.1

--- frontend/src/views/UploadImage.vue
+++ frontend/src/views/UploadImage.vue
@@ -11,20 +11,36 @@ const formData = ref({
 const image = ref(null);
 const uploading = ref(false);
 const upload_error = ref(false);
+const preview = ref(false);
+const preview_image = ref(null);
+const open_preview_modal = ref(false);
+
 
 
 // Select Images
 const onFileSelected = (event) => {
     if (!event.target.files || event.target.files.length > 1) return;
     let selected = event.target.files[0];
-    if (selected.size && selected.size > 2000000) return [this.toast('Image size too large (Size <= 2Mb)', 'error'), clearImage()] // Image size shouldn't exceed 2Mb (2*10^6 bytes)
+    if (selected.size && selected.size > 5000000) return [notify('Image size too large (Size <= 5Mb)', 'error'), clearImage()] // Image size shouldn't exceed 2Mb (2*10^6 bytes)
     image.value = selected;
-    return;
+    return readImage(selected);
 };
 
 // Clear Image and Reader
 const clearImage = () => {
     image.value = null;
+    preview_image.value = null;
+    return;
+};
+
+// Image Reader
+const readImage = (image) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = (event) => {
+        preview_image.value = event.target.result;
+    };
+    preview.value = true;
     return;
 };
 
@@ -49,7 +65,6 @@ const createBlog = async () => {
             },
         })
         .then((response) => {
-
             console.log(response)
             notify("Post created !", "success");
             uploading.value = false;
@@ -58,7 +73,6 @@ const createBlog = async () => {
 
         })
         .catch((error) => {
-            console.log('error', error)
             upload_error.value = true
             uploading.value = false
             var message = "Something went wrong !";
@@ -86,6 +100,12 @@ const createBlog = async () => {
         <!-- Upload form -->
         <div class="flex justify-center my-6">
             <form class="w w-64">
+
+                <!-- Preview Image -->
+                <button type="button" @click="open_preview_modal = !open_preview_modal" v-if="preview"
+                    class="mb-6 flex justify-center">
+                    <img class="inline-block w-72 h-64" :src="(preview_image) ? preview_image : 'images/avatar.svg'" />
+                </button>
                 <!-- Image -->
                 <div class="mb-6">
                     <label for="image"
@@ -121,5 +141,33 @@ const createBlog = async () => {
                     (uploading) ? 'uploading ...' : 'upload' }}</button>
             </form>
         </div>
+
+        <!--Modal  -->
+        <div v-if="open_preview_modal" class="relative z-10" aria-labelledby="modal-title" role="dialog"
+            aria-modal="true">
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <div class="fixed inset-0 z-10 overflow-y-auto">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div
+                        class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                            <div class="sm:flex sm:items-start">
+                                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                    <div class="mb-6 flex justify-center">
+                                        <img class="inline-block w-full h-full"
+                                            :src="(preview_image) ? preview_image : 'images/avatar.svg'" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                            <button type="button" @click="open_preview_modal = !open_preview_modal"
+                                class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--Modal Ends  -->
     </section>
 </template>
